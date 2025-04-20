62fa45ba1e285d5adbc9b4d6d1d9af866990223e
Ted Bjurlin <teddybjurlin@proton.me>
2025-04-20 18:52:54 -0500

    added additional copy traits

--- src/lib.rs
+++ src/lib.rs
@@ -184,6 +184,7 @@ impl<const HEAP_SIZE: usize> RamHeap<HEAP_SIZE> {
     }
 }
 
+#[derive(Copy, Clone, Debug)]
 pub struct OnceAndDoneHeap<const HEAP_SIZE: usize, const MAX_BLOCKS: usize> {
     heap: RamHeap<HEAP_SIZE>,
     block_info: BlockTable<MAX_BLOCKS>,
@@ -244,7 +245,8 @@ impl<const HEAP_SIZE: usize, const MAX_BLOCKS: usize> GarbageCollectingHeap
 
     fn assert_no_strays(&self) {}
 }
-#[derive(Copy, Clone)]
+
+#[derive(Copy, Clone, Debug)]
 pub struct CopyingHeap<const HEAP_SIZE: usize, const MAX_BLOCKS: usize> {
     heaps: [RamHeap<HEAP_SIZE>; 2],
     block_info: BlockTable<MAX_BLOCKS>,
@@ -380,6 +382,7 @@ pub struct GenerationalHeap<
     active_gen_0: usize,
     active_gen_1: usize,
 }
+
 
 impl<const HEAP_SIZE: usize, const MAX_BLOCKS: usize, const MAX_COPIES: usize>
     GenerationalHeap<HEAP_SIZE, MAX_BLOCKS, MAX_COPIES>
